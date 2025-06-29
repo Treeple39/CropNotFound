56fd764dffad55c4898636a89fe5df64bc46c136
freesilly <101262743+freesilly@users.noreply.github.com>
2025-06-29 09:50:11 +0800

    233

--- Assets/_Project/Scripts/UI/ShowScore_EndScene.cs
+++ Assets/_Project/Scripts/UI/ShowScore_EndScene.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowScore_EndScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Text _text;
+    
     void Start()
     {
-        
+        _text = GetComponent<Text>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        ShowScore();
+    }
+
+    void ShowScore()
+    {
+        _text.text = Score.score.ToString();
     }
 }
