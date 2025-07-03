ef301c2d047a9ce6f1b384ef405d7e77833790a5
SeiyunSky <1447909240@qq.com>
2025-07-03 19:00:00 +0800

    跳过已观看的动画

--- Assets/_Project/Scripts/Item/Score.cs
+++ Assets/_Project/Scripts/Item/Score.cs
@@ -8,9 +8,16 @@ public static class Score
 
     public static int itemCount = 0;
 
+    public static bool skipAnim = false;
+
     public static void ResetScore()
     {
         score = 0;
         itemCount = 0;
+    }
+
+    public static void AnimSkip()
+    {
+        skipAnim = true;
     }
 }
